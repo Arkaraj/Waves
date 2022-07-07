19be9978a68dc1b554c53c321b26f808a4150283
Arkaraj <arkaraj2017@gmail.com>
2022-07-07 14:20:15 +0530

    react auth services and hocs added :rocket:

---
+++ client/src/Services/AuthService.js
@@ -0,0 +1,45 @@
+// let apiUrl = "api/v1"
+// eslint-disable-next-line
+export default {
+  register: async (user) => {
+    const res = await fetch("/users/register", {
+      method: "POST",
+      body: JSON.stringify(user),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+    return data;
+  },
+  login: async (user) => {
+    const res = await fetch("/users/login", {
+      method: "POST",
+      body: JSON.stringify(user),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    // if (res.status !== 401) return res.json().then((data) => data);
+    // // We get the user data
+    // else return { isAuthenticated: false };
+    const data = await res.json();
+    return data;
+  },
+  logout: async () => {
+    const res = await fetch("/users/logout", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+    return data;
+  },
+  // Sync backend and front end
+  isAuthenticated: async () => {
+    const res = await fetch("/users/auth");
+    if (res.status !== 401) return res.json().then((data) => data);
+    else return { isAuthenticated: false, user: {} };
+  },
+};
